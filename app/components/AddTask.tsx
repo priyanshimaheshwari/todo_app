66ce12c43f16cbd270b853066498266cdfc4435a
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-06-08 01:16:23 +0530

    add task

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import {AiOutlinePlus} from 'react-icons/ai'
+import Modal from './Modal'
+import { useState } from 'react';
+
 const AddTask = () => {
+    const [modalOpen , setModalOpen] = useState<boolean>(false);
   return (
     <div>
-        <button className="btn btn-primary w-full">Add new task</button>
+        <button onClick={()=>setModalOpen(true)} className="btn btn-primary w-full">Add new task
+        <AiOutlinePlus className='ml-2' size={18}/>
+            </button>
+            <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+                <form>
+                    <h3 className='font-bold text-lg'>Add new task</h3>
+                    <div className='modal-action'>
+                    <input type="text" placeholder='Type here'
+                    className='input input-bordered w-full '/>
+                    <button type="submit" className='btn'>Submit</button>
+                    </div>
+                </form>
+                </Modal>
     </div>
   )
 }
---
+++ app/components/Task.tsx
@@ -0,0 +1,16 @@
+import { ITask } from "@/types/tasks"
+
+interface TaskProps {
+    task:ITask
+}
+
+const Task : React.FC<TaskProps> = ({task}) => {
+  return (
+    <tr key={task.id}>
+          <td>{task.text}</td>
+          <td>Quality Control Specialist</td>
+        </tr>
+  )
+}
+
+export default Task
